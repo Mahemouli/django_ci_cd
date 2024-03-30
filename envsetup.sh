2dc89d43a69e80a85b4346ff7fd9ec073388cfef
Mahemouli <137928072+Mahemouli@users.noreply.github.com>
2024-03-30 19:09:31 +0530

    Create envsetup.sh

---
+++ envsetup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -d "env" ] 
+then
+    echo "Python virtual environment exists." 
+else
+    python3 -m venv env
+fi
+
+source env/bin/activate
+
+
+pip3 install -r requirements.txt
+
+if [ -d "logs" ] 
+then
+    echo "Log folder exists." 
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
