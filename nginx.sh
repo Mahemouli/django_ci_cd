9a90a860f08086dbeb906f8d521c18c75a5b8d97
Mahemouli <137928072+Mahemouli@users.noreply.github.com>
2024-03-31 16:11:38 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp -rf /scripts/app.conf /etc/nginx/sites-available/app
+sudo cp -rf scripts/app.conf /etc/nginx/sites-available/app
 chmod 710  /var/lib/jenkins/workspace/Pipeline-tools
 
 sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
