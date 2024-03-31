fb69e8f63e31c51bfc8dc3051c4267977fbc6b1c
Mahemouli <137928072+Mahemouli@users.noreply.github.com>
2024-04-01 05:16:44 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -3,7 +3,7 @@
 sudo cp -rf scripts/app.conf /etc/nginx/sites-available/dev/app
 chmod 710  /var/lib/jenkins/workspace/Pipeline-tools
 
-sudo ln -s /etc/nginx/sites-available/dev/app /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/dev/app /etc/nginx/sites-enabled/dev
 sudo nginx -t
 
 sudo systemctl start nginx
