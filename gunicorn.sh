c963123bf3272a211fce7ecebd860be4ebd5485a
Mahemouli <137928072+Mahemouli@users.noreply.github.com>
2024-03-31 14:59:35 +0530

    Update gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -4,9 +4,9 @@ source env/bin/activate
 
 cd /var/lib/jenkins/workspace/Pipeline-tools/app
 
-sudo python3 manage.py makemigrations
-sudo python3 manage.py migrate
-sudo python3 manage.py collectstatic -- no-input
+python3 manage.py makemigrations
+python3 manage.py migrate
+#python3 manage.py collectstatic -- no-input
 
 echo "Migrations done"
 
